97529d5d27c137c3c9851eb752807a37015cc81d
user7 <im.krista69@gmail.com>
2025-05-21 14:13:26 +0300

    update dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+/* eslint-disable */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-// import store from './store';
 import { store } from './rtk';
 import './index.scss';
 import App from './components/app/App';
